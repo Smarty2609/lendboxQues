6264b00bf33e6e8eebb96277b8a5b405bf1b6a57
Smarty2609 <saini.shivam9286@gmail.com>
2024-02-02 21:15:06 +0530

    comments added

--- src/customHooks/useComponentObserver.js
+++ src/customHooks/useComponentObserver.js
@@ -3,19 +3,16 @@ import React, {useState} from "react";
 const useComponentObserver = () =>{
     const [enableScroll, setEnableScroll] = useState(false);
   
-      const observer = new IntersectionObserver(
-        ([entry]) => {
-          if (entry.isIntersecting) {
-            setEnableScroll(true);
-          }else{
-            setEnableScroll(false);
-          }
-        },
-        { threshold: 0 }
-      );
-    
+    const observer = new IntersectionObserver(
+    ([entry]) => {
+        if (entry.isIntersecting) {  // if required component is in window view
+        setEnableScroll(true);
+        }else{
+        setEnableScroll(false);
+        }
+    },{ threshold: 0 }
+    );
     return [enableScroll, observer];
-    
   }
 
  export default useComponentObserver; 
--- src/utils/customScroll.js
+++ src/utils/customScroll.js
@@ -1,30 +1,31 @@
 export const handleCustomWheel = (event,section,container,scrollContainer) => {
     const windowSection = section?.current;
     const content = document.querySelector(container);
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const contentTop = content?.offsetTop;
-      const contentBottom = contentTop + content?.offsetHeight;
-      if (contentTop >= (scrollPosition + windowHeight / 2.65) || contentBottom <= (scrollPosition + windowHeight / 1.4 )) {
+    const contentTop = content?.offsetTop;
+    const contentBottom = contentTop + content?.offsetHeight;
+   
+    const scrollPosition = window.scrollY;
+    const windowHeight = window.innerHeight;
+     
+    // enable vertical scrolling if required comp is not in view
+    if (contentTop >= (scrollPosition + windowHeight / 2.65) || contentBottom <= (scrollPosition + windowHeight / 1.4 )) {
+      document.body.style.overflowY = '';
+    } else {
+      document.body.style.overflowY = 'hidden'; // disable vertical scroll
+      if(document.querySelector(scrollContainer)?.style?.overflowX){
+        document.querySelector(scrollContainer).style.overflowX="scroll"; // enable horizontal scroll
+      }
+
+      // enable vertical scrolling when horizontal scroll is at start or end position
+      const isAtEnd = windowSection.scrollLeft === windowSection?.scrollWidth - windowSection?.clientWidth;
+      if (windowSection?.scrollLeft === 0 || isAtEnd) {
         document.body.style.overflowY = '';
-      } else {
-        document.body.style.overflowY = 'hidden';
-        if(document.querySelector(scrollContainer)?.style?.overflowX){
-          document.querySelector(scrollContainer).style.overflowX="scroll";
-        }
-        if (windowSection?.scrollLeft === 0) {
-          document.body.style.overflowY = '';
-        }
-        
-        const isAtEnd = windowSection.scrollLeft === windowSection?.scrollWidth - windowSection?.clientWidth;
-        if (isAtEnd) {
-          document.body.style.overflowY = '';
-        }
+      }
     }
     
     if (event?.deltaY < 0) {
-      windowSection.scrollLeft -= 15;
+      windowSection.scrollLeft -= 15;  // scroll towards right on scrolling downward
     } else {
-      windowSection.scrollLeft += 15;
+      windowSection.scrollLeft += 15; // scroll towards left on scrolling upward
     }
   };
